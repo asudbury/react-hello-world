f927706646030b0a74c094dafb99d564b4a63b73
Adrian Sudbury <sudburya@gmail.com>
2020-06-23 08:10:36 +0100

    added material design elements

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Fade from '@material-ui/core/Fade';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
+    <Fade in="true" timeout={10000}>
+          <Container align="right" className="container">
+            <Fade in="true" timeout={16000}>
+                <Typography variant="h1">Hello World</Typography>
+            </Fade>
+            <Fade in="true" timeout={22000}>
+              <Typography variant="h5">by Adrian Sudbury</Typography>
+            </Fade>
+            <Fade in="true" timeout={30000}>
+              <Button variant="contained" color="primary" href="https://github.com/asudbury/react-hello-world" target="_blank" rel="noopener noreferrer">
+                Source Code
+              </Button>
+            </Fade> 
+          </Container>
+    </Fade>
 
-        <p>
-            <small>
-              <a href='https://github.com/asudbury/react-hello-world' target="_blank" rel="noopener noreferrer">Source Code</a>
-            </small>
-        </p>
-        <p>
-            <small>
-              By Adrian Sudbury
-            </small>
-        </p>
-      </header>
-    </div>
   );
 }
 
