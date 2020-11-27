ac0d9e032daf51b39226c4ec7f0644b85a100b70
Adrian Sudbury <sudburya@gmail.com>
2020-11-27 16:16:37 +0000

    Added Eslint, Prettier and AirBnb Style guide

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ import './App.css';
 function App() {
   return (
     <Fade in="true" timeout={10000}>
-          <Container align="right" className="container">
-            <Fade in="true" timeout={16000}>
-                <Typography variant="h1">Hello World</Typography>
-            </Fade>
-            <Fade in="true" timeout={22000}>
-              <Typography variant="h5">by Adrian Sudbury</Typography>
-            </Fade>
-            <Fade in="true" timeout={30000}>
-              <Button variant="contained" color="primary" href="https://github.com/asudbury/react-hello-world" target="_blank" rel="noopener noreferrer">
-                Source Code
-              </Button>
-            </Fade> 
-          </Container>
+      <Container align="right" className="container">
+        <Fade in="true" timeout={16000}>
+          <Typography variant="h1">Hello World</Typography>
+        </Fade>
+        <Fade in="true" timeout={22000}>
+          <Typography variant="h5">by Adrian Sudbury</Typography>
+        </Fade>
+        <Fade in="true" timeout={30000}>
+          <Button
+            variant="contained"
+            color="primary"
+            href="https://github.com/asudbury/react-hello-world"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source Code
+          </Button>
+        </Fade>
+      </Container>
     </Fade>
-
   );
 }
 
